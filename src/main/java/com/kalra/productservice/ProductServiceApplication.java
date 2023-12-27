219555eaa281a9ec58d972587a403cd453ebd988
Sahil Kalra <sahil.kalra@pason.com>
2023-12-27 07:46:23 -0700

    add necessary files

--- src/main/java/com/kalra/productservice/ProductServiceApplication.java
+++ src/main/java/com/kalra/productservice/ProductServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class App
+public class ProductServiceApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(ProductServiceApplication.class, args);
     }
 }
